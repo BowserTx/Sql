20def0aa33a66b3c04ac42bdb695ad611d24af0b
Willian Souza <76187694+BowserTx@users.noreply.github.com>
2021-03-28 20:43:00 -0400

    Uri

---
+++ 2738 - Concurso.sql
@@ -0,0 +1,6 @@
+select candidate.name, round(((score.math*2.0+score.specific*3.0+score.project_plan*5.0)/10.0),2) as avg
+from candidate
+  	join score
+	on score.candidate_id = candidate.id
+	
+	order by avg desc;
